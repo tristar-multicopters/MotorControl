8a559fa7e56a9a26badaa377c8e165cb66766560
JAPolo2 <jorge.polo@ftex.ca>
2022-03-17 22:32:48 -0400

    Add Evionics module and header

---
+++ src/GNR1_Firmware/rev_E/NRF/MD_MASTER/FTEX_MODULES/Inc/evionics.h
@@ -0,0 +1,16 @@
+/**
+  ******************************************************************************
+  * @file    evionics.h
+	* @author  Jorge Andres Polo, FTEX
+  * @brief   Header of evionics.c module that controls communication between host  				 
+  *					 (Evionics interface) and the GNR	
+	******************************************************************************
+	*/
+
+#include "storage_management.h" // it includes vc_interface which includes euart_manager.h
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __EVIONICS_H
+#define __EVIONICS_H
+
+#endif
---
+++ src/GNR1_Firmware/rev_E/NRF/MD_MASTER/FTEX_MODULES/Src/evionics.c
@@ -0,0 +1,11 @@
+/**
+  ******************************************************************************
+  * @file    evionics.c
+  * @author  Jorge Andres Polo, FTEX
+  * @brief   High level module that controls communication between host (Evionics interface) 				 
+  *					 and the GNR	
+	******************************************************************************
+	*/
+	
+	#include "evionics.h"
+	
