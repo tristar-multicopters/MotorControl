90b32db0e9ddb5aa39a9247f1336358e2304807f
Roneyoo <77132809+Roneyoo@users.noreply.github.com>
2022-03-29 09:53:11 -0400

    Added Grizzly support and corrected motor selection bug

--- src/GNR1_Firmware/rev_E/NRF/MD_MASTER/FTEX_MODULES/Src/vc_config.c
+++ src/GNR1_Firmware/rev_E/NRF/MD_MASTER/FTEX_MODULES/Src/vc_config.c
@@ -115,6 +115,20 @@ THRO_Handle_t ThrottleHandle =
 		.bSlopeTorque = -7,
 		.bDivisorTorque = 25,
 	}
+		#elif VEHICLE_SELECTION == VEHICLE_GRIZZLY
+	.hParam =
+	{
+		.hLowPassFilterBW1 = 8,
+		.hLowPassFilterBW2 = 2,
+	
+		.hOffsetThrottle = 9900,
+		.bSlopeThrottle = 5,
+		.bDivisorThrottle = 3,
+		
+		.hOffsetTorque = 4000,
+		.bSlopeTorque = 8,
+		.bDivisorTorque = 48,
+	}
 	#else
 	.hParam =
 	{
@@ -198,6 +212,9 @@ MS_Handle_t MotorSelectorHandle =
 	#elif VEHICLE_SELECTION == VEHICLE_EBGO
 	.bIsInvertedLogic = false,
 	.bMSEnable = false,
+		#elif VEHICLE_SELECTION == VEHICLE_GRIZZLY
+	.bIsInvertedLogic = false,
+	.bMSEnable = false,
 	#else
 	.bIsInvertedLogic = false,
 	.bMSEnable = true,
@@ -269,6 +286,18 @@ DRVT_Handle_t DrivetrainHandle =
 	.hStartingThrottle = 1000,
 	.hStoppingThrottle = 500,
 	.hStoppingSpeed = 0,
+		#elif VEHICLE_SELECTION == VEHICLE_GRIZZLY
+	.bUseMotorM1 = true,
+	.bUseMotorM2 = false,
+	.bDefaultMainMotor = M1,
+	.bCtrlType = TORQUE_CTRL,
+	.hTorqueRampTimeUp = 200,
+	.hTorqueRampTimeDown = 50,
+	.hSpeedRampTimeUp = 200,
+	.hSpeedRampTimeDown = 50,
+	.hStartingThrottle = 1000,
+	.hStoppingThrottle = 500,
+	.hStoppingSpeed = 0,
 	#else
 	.bUseMotorM1 = true,
 	.bUseMotorM2 = false,
@@ -297,7 +326,7 @@ VCI_Handle_t VCInterfaceHandle =
 	.pDrivetrain = &DrivetrainHandle,
 };
 
-eUART_protocol_t EUART_handle_t = EUART_EVIONICS; // Has to been initialise by Evionics first
+eUART_protocol_t EUART_handle_t = EUART_APT; // Has to been initialise by Evionics first
 //LCD_handle_t BafangScreenHandle = 
 //{
 //	.pVCInterface = &VCInterfaceHandle,
