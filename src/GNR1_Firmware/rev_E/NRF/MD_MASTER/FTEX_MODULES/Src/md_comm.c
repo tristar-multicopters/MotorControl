16eadda844e888a8f57fbdb496674ef0ed100141
Andy Beaudoin <andy.beaudoin@Ftex.ca>
2022-03-31 12:15:14 -0400

    Added debug flags for md_comm

--- src/GNR1_Firmware/rev_E/NRF/MD_MASTER/FTEX_MODULES/Src/md_comm.c
+++ src/GNR1_Firmware/rev_E/NRF/MD_MASTER/FTEX_MODULES/Src/md_comm.c
@@ -11,6 +11,7 @@
 
 
 #include "md_comm.h"
+#define MDCOMM_DEBUG_FLAGS   //Used to enable or disable md_comm debugging flags   
 
 static MD_Comm_Handle_t * m_pMDcomm;
 static UFCP_Handle_t * m_pMDcomm_ufcp;
@@ -76,8 +77,11 @@ static void md_frame_event_handler(ufcp_evt_t * p_ufcp_event)
 			
 			case UFCP_CRC_ERROR:
 					m_pMDcomm->hStatus &= ~MDCOMM_TRANSFER_ONGOING;
-					m_pMDcomm->hError |= MDCOMM_BAD_CRC;
-					break;
+					
+			 #ifdef MDCOMM_DEBUG_FLAGS
+			    m_pMDcomm->hError |= MDCOMM_BAD_CRC;
+				#endif	
+			    break;
 			
 			default:
 					break;
@@ -181,10 +185,14 @@ static void md_frame_received_protocol(FCP_Frame_t * tx_frame, FCP_Frame_t * rx_
 				}
 				break;
 		case ACK_ERROR:
+			#ifdef MDCOMM_DEBUG_FLAGS
 				m_pMDcomm->hError |= MDCOMM_ACK_ERROR;
+		  #endif
 				break;
 		default:
+			#ifdef MDCOMM_DEBUG_FLAGS  
 				m_pMDcomm->hError |= MDCOMM_UNEXPECTED;
+		  #endif
 				break;
 	}
 }
