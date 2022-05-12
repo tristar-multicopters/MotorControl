d26437508a440c6b658a221e4befd0bb83fc3e66
sabou267 <sami.bouzid.1@tristarmulticopters.com>
2022-05-12 13:02:27 -0400

    GNR2 project start update

--- src/GNR2_Firmware/revA/grn2_ra6t2/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h
+++ src/GNR2_Firmware/revA/grn2_ra6t2/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h
@@ -2,6 +2,10 @@
 #ifndef BSP_PIN_CFG_H_
 #define BSP_PIN_CFG_H_
 #include "r_ioport.h"
+
+/* Common macro for FSP header files. There is also a corresponding FSP_FOOTER macro at the end of this file. */
+FSP_HEADER
+
 #define M1_BKIN_OCD1 (BSP_IO_PORT_00_PIN_00)
 #define REVERSE (BSP_IO_PORT_00_PIN_01)
 #define M2_CURR_V_RET (BSP_IO_PORT_00_PIN_02)
@@ -89,4 +93,8 @@
 extern const ioport_cfg_t g_bsp_pin_cfg; /* R7FA6T2BB3CFP.pincfg */
 
 void BSP_PinConfigSecurityInit();
+
+/* Common macro for FSP header files. There is also a corresponding FSP_HEADER macro at the top of this file. */
+FSP_FOOTER
+
 #endif /* BSP_PIN_CFG_H_ */
