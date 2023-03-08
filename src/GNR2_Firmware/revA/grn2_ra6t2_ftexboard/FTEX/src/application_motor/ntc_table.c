98c3b319ff636b01db098bb4b8a22c1ef5db1209
Krupali123b <116736150+Krupali123b@users.noreply.github.com>
2023-03-08 14:11:42 -0500

    Revised the NTC lookup table (#248)
    * Revised the NTC lookup table
    
    made changes to ntc_table.c file.
    
    * Delete gnr2_ra6t2.uvguix.krupali.bhatt_ftex
    
    * Delete RTX_Config.c.base@5.1.1
    
    * Delete startup_RA.c.base@1.0.0
    
    * Delete system_RA.c.base@1.0.0
    
    * Delete RTX_Config.h.base@5.5.2
    
    * Added comments
    
    Commented on the changes made to the file.
    
    * Added relevant comments
    
    Added relevant comments.

--- src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/src/application_motor/ntc_table.c
+++ src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/src/application_motor/ntc_table.c
@@ -1,43 +1,59 @@
 /**
-    * @file  ntc_table.c
-    * @author Sami Bouzid, FTEX inc.
-    * @brief This file declares the NTC (negative temperature coefficient) thermistor lookup table and its parameters to use with LookupTable component.
-    *
+
+* @file  ntc_table.c
+* @author Luiz Martins, FTEX inc.
+* @brief This file declares the NTC (negative temperature coefficient) thermistor lookup table and its parameters to use with LookupTable component.
+* Added practical drift after thermal camera feedback
 */
 
 #include "ntc_table.h"
 
-#define NTC_LUT_SIZE                        19
-#define NTC_LUT_DIGITAL_STEP                2000
-#define NTC_LUT_DIGITAL_FIRST_VALUE         25000
+#define NTC_LUT_SIZE                        31           // the number of correlation elements on this table
+#define NTC_LUT_DIGITAL_STEP                1600         //100 is the tics steps for a 12bits ADC conversion
+#define NTC_LUT_DIGITAL_FIRST_VALUE         16000        //1000 is the first value considered in the table that has a temperature correlation (7 degree C).
+                                                         //*16 is the factor on tics conversion due variable casting
 
-const int32_t NTCTemperatureTable[NTC_LUT_SIZE] = {
-    20,
-    22,
-    24,
-    26,
-    28,
+
+
+const int32_t NTCTemperatureTable[NTC_LUT_SIZE] =        //revised the function to calculate temperature values after the NTC test.
+{
+    7,                                                   //Added few values to check the behaviour for low temperature values.
+    9,            
+    11,
+    13,
+    15,
+    17,
+    19,
+    21,
+    23,
+    25,
+    27,
+    29,
     31,
-    34,
+    33,
+    35,
     37,
-    40,
+    39,
+    41,
     44,
-    48,
+    46,
+    49,
     52,
-    57,
-    62,
-    67,
+    55,
+    59,
+    63,
+    68,
     73,
     80,
-    87,
-    95,
+    89,
+    103,
+    129,
 };
 
-LookupTableHandle_t NTCLookupTable = 
-{
-  .hXDataStep = NTC_LUT_DIGITAL_STEP,
-  .wXDataFirstValue = NTC_LUT_DIGITAL_FIRST_VALUE,
-  .hTableLength = NTC_LUT_SIZE,
-  .pOutputTable = NTCTemperatureTable,
+LookupTableHandle_t NTCLookupTable =
+ {
+    .hXDataStep = NTC_LUT_DIGITAL_STEP,
+    .wXDataFirstValue = NTC_LUT_DIGITAL_FIRST_VALUE,
+    .hTableLength = NTC_LUT_SIZE,
+    .pOutputTable = NTCTemperatureTable,
 };
-
