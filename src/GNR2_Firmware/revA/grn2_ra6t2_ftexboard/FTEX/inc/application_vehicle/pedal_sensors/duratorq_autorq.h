7abb35d50e56a360e60284c66984e13d00e987c4
brunoftex <118211624+brunoftex@users.noreply.github.com>
2023-09-14 09:21:10 -0400

    adjusted torque sensor range to revd (#443)

--- src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/inc/application_vehicle/pedal_sensors/duratorq_autorq.h
+++ src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/inc/application_vehicle/pedal_sensors/duratorq_autorq.h
@@ -7,9 +7,9 @@
 
 #define PTS_FILTER_ALPHA                2.27F   // Butterworth alpha coefficient pedal torque sensor filtering
 #define PTS_FILTER_BETA                 -0.27F  // Butterworth beta coefficient pedal torque sensor filtering
-#define PTS_MAX_PTSVALUE                52000   // Maximum analog value to reach
+#define PTS_MAX_PTSVALUE                61000   // Maximum analog value to reach
 
-#define PTS_OFFSET_ADC2PTS              24000   // Offset for ADC to pedal torque sensor linear transformation
+#define PTS_OFFSET_ADC2PTS              16000   // Offset for ADC to pedal torque sensor linear transformation
 
 /***************** PEDAL TORQUE SENSOR CADENCE PARAMETERS  ******************************/
 
