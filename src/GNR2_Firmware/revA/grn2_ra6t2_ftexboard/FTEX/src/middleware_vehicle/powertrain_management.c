8d264f57692a7fd2ecef0c70d2075b6e73cc3574
brunoftex <118211624+brunoftex@users.noreply.github.com>
2023-09-14 13:37:14 -0400

    Rested pass detection when brake is pressed (#446)

--- src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/src/middleware_vehicle/powertrain_management.c
+++ src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/src/middleware_vehicle/powertrain_management.c
@@ -129,6 +129,10 @@ void PWRT_CalcMotorTorqueSpeed(PWRT_Handle_t * pHandle)
         if (bIsBrakePressed)
         {
             hAux = 0;
+            
+            //Reset passed detection
+            pHandle->pPAS->bPASDetected = false;
+            
             // Reset All the Pedal Assist Parameters
             PedalAssist_ResetParameters(pHandle->pPAS);
             PWRT_ForceDisengageCruiseControl(pHandle);
