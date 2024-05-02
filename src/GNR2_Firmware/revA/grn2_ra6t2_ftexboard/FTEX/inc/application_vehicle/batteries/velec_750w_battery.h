35416ab7f17a45a7a2bed3db8fd8c755f09a1cdd
D'herbecourt Theodore <101578429+Theodoredherbecourt@users.noreply.github.com>
2024-05-02 15:36:29 -0400

    Update velec_750w_battery.h

--- src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/inc/application_vehicle/batteries/velec_750w_battery.h
+++ src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/inc/application_vehicle/batteries/velec_750w_battery.h
@@ -4,7 +4,7 @@
 /******************************** BATTERY MONITORING PARAMETERS ******************************/
 
 #define BATTERY_FULL_VOLT_X_100   5700
-#define BATTERY_EMPTY_VOLT_X_100  3800
+#define BATTERY_EMPTY_VOLT_X_100  3200
 
 #define BATTERY_SOC_LOW_PERCENT      5  // Battery SOC in % for which we set the battery low flag (stops powertrain form pushing power)
 #define BATTERY_SOC_OK_PERCENT      25  // Battery SOC in % for which we clear the battery low flag
@@ -15,7 +15,7 @@
 #define POWER_LIMIT_REF                     MAX_CURRENT_LIMIT   // Defines if the code should use MAX_APPLICATION_POSITIVE_POWER or MAX_APPLICATION_CURRENT
 #define MAX_APPLICATION_POSITIVE_POWER      1100                // Maximum power in watts that drive can push to the motor
 #define MAX_APPLICATION_NEGATIVE_POWER      1100                // Maximum power in watts that drive can accept from the motor
-#define MAX_APPLICATION_CURRENT             25                  // Maximum battery current in amps that drive can accept from the motor
+#define MAX_APPLICATION_CURRENT             20                  // Maximum battery current in amps that drive can accept from the motor
 
 /******************************** BATTERY  PARAMETERS ******************************/
 
