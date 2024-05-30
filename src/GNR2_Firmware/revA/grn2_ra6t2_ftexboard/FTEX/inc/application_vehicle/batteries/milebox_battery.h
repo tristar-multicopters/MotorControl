726b8ef91b06a79ae2b886dcfd9999ee7f699d29
natashaftex <142815548+natashaftex@users.noreply.github.com>
2024-05-30 16:52:58 -0400

    increased milebox empty battery number (#648)

--- src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/inc/application_vehicle/batteries/milebox_battery.h
+++ src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/inc/application_vehicle/batteries/milebox_battery.h
@@ -4,7 +4,7 @@
 /******************************** BATTERY MONITORING PARAMETERS ******************************/
 
 #define BATTERY_FULL_VOLT_X_100   5200
-#define BATTERY_EMPTY_VOLT_X_100  3400
+#define BATTERY_EMPTY_VOLT_X_100  3950
 
 #define BATTERY_SOC_LOW_PERCENT      1  // Battery SOC in % for which we set the battery low flag (stops powertrain form pushing power)
 #define BATTERY_SOC_OK_PERCENT      25  // Battery SOC in % for which we clear the battery low flag
