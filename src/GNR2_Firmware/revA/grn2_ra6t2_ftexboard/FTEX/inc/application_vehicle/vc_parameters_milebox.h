839a09b4a8a5b1f6bf5290fbbb55add060e88106
nicolas-demers-ftex <nicolas.demers@ftex.ca>
2024-06-25 17:01:47 -0400

    Initial PAS Tuning

--- src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/inc/application_vehicle/vc_parameters_milebox.h
+++ src/GNR2_Firmware/revA/grn2_ra6t2_ftexboard/FTEX/inc/application_vehicle/vc_parameters_milebox.h
@@ -37,7 +37,7 @@
 #include "pedal_sensors/duratorq_autorq.h"
 
 /***************** TORQUE SENSOR FEEL PARAMETERS  ******************************/
-#define PTS_OFFSET_PTS2TORQUE_STARTUP       80              // Offset for pedal torque sensor to torque linear transformation during the startup in %
+#define PTS_OFFSET_PTS2TORQUE_STARTUP       10              // Offset for pedal torque sensor to torque linear transformation during the startup in %
 #define PTS_OFFSET_STARTUP_SPEED_KMH         1              // Speed under which the Startup pedal torque sensor offset is used in km/h
 #define PTS_OFFSET_PTS2TORQUE               10              // Offset for pedal torque sensor to torque linear transformation in %
 #define PTS_OFFSET_PTS2TORQUE_SAFETY        40              // Offset for pedal torque sensor to torque linear transformation that is considered safe in %
